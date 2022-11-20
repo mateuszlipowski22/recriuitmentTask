4b75e849bf2927dda0009893c7645c37a761bfd4
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-11-21 00:24:57 +0100

    add Teacher and Student data transfer objects

---
+++ src/main/java/ultimate/systems/recruitmenttask/dto/StudentDTO.java
@@ -0,0 +1,41 @@
+package ultimate.systems.recruitmenttask.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ultimate.systems.recruitmenttask.models.Teacher;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDTO {
+
+
+    private Long id;
+
+    @NotNull
+    @Size(min = 2)
+    private String name;
+
+    private String surname;
+
+    @NotNull
+    @Min(value = 19)
+    private Integer age;
+
+    @NotNull
+    @Email
+    private String email;
+
+    private String specialization;
+
+    private List<Teacher> teachers;
+}
---
+++ src/main/java/ultimate/systems/recruitmenttask/dto/TeacherDTO.java
@@ -0,0 +1,40 @@
+package ultimate.systems.recruitmenttask.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ultimate.systems.recruitmenttask.models.Student;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeacherDTO {
+
+    private Long id;
+
+    @NotNull
+    @Size(min = 2)
+    private String name;
+
+    private String surname;
+
+    @NotNull
+    @Min(value = 19)
+    private Integer age;
+
+    @NotNull
+    @Email
+    private String email;
+
+    private String subject;
+
+    private List<Student> students;
+}
